5c2658588bc7e3c8e8a6b660f53459c193fcf0ea
Seba451 <sebaversa02@gmail.com>
2025-09-10 21:52:03 -0300

    sacar eslint blocker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ❌ No bloquees el build por errores de ESLint
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
